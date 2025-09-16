2558b52ff361d074a6c90e0507bb749fa0b1d141
unknown <akash.dada@testyantra.in>
2025-09-16 23:00:20 +0530

    added room service

---
+++ devchat-room/src/test/java/com/devchat/devchat_room/DevchatRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devchat.devchat_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevchatRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
